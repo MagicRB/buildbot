45f68c59505412696c2f70daaae1814036d7cef1
magic_rb <richard@brezak.sk>
2025-01-15 17:06:03 +0100

    Fix uses of `db['db_url']`
    Signed-off-by: magic_rb <richard@brezak.sk>

--- master/buildbot/test/unit/db/test_connector.py
+++ master/buildbot/test/unit/db/test_connector.py
@@ -53,7 +53,7 @@ class TestDBConnector(TestReactorMixin, unittest.TestCase):
 
     @defer.inlineCallbacks
     def startService(self, check_version=False):
-        self.master.config.db['db_url'] = self.db_url
+        self.master.config.db.db_url = self.db_url
         yield self.db.setup(check_version=check_version)
         yield self.db.startService()
         yield self.db.reconfigServiceWithBuildbotConfig(self.master.config)
